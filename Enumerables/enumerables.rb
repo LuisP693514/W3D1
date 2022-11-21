d1eb7dc084d79cdc201c4d130f57cb689e2133b2
Luis <perezluis167@gmail.com>
2022-11-21 15:30:00 -0500

    Updated my_any etc

--- Enumerables/enumerables.rb
+++ Enumerables/enumerables.rb
@@ -6,19 +6,53 @@ class Array
             blk.call(self[i])
             i += 1 
         end 
+        self
     end 
 
 
     def my_select(&blk)
         new_arr = []
-        new_arr << self.my_each(&blk)
+        self.my_each do |ele|
+
+            new_arr << blk.call(ele) if blk.call(ele)
+
+        end
+
         return new_arr
     end 
 
 
     def my_reject(&blk)
         new_arr = []
+
+        (0...self.length).each do |idx|
+
+            new_arr << self[idx] if !blk.call(self[idx])
+
+        end
+        new_arr
+
     end 
+
+    def my_any?(&blk)
+
+        (0...self.length).each do |idx|
+
+            return true if blk.call(self[idx])
+
+        end
+        false
+    end
+
+    def my_all?(&blk)
+
+        (0...self.length).each do |idx|
+
+            return false if !blk.call(self[idx])
+
+        end
+        true
+    end
 
 end 
 
@@ -27,4 +61,12 @@ end
 arr = [1, 2, 4]
 # arr.my_each {|ele| p ele + 1}
 
-p arr.my_select {|ele|  ele if ele > 2}
+# p arr.my_select {|ele|  ele if ele > 2}
+
+# p arr.my_reject {|ele| ele > 2}
+
+# p arr.my_any? {|ele| ele > 5} # false
+# p arr.my_any? {|ele| ele > 3} # true
+
+p arr.my_all? {|ele| ele < 5} # true
+p arr.my_all? {|ele| ele > 3} # false
