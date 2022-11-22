96114ca4745807a52699aa02035f55c59dfed223
Luis <perezluis167@gmail.com>
2022-11-22 04:19:17 -0500

    Fixed my_zip

--- Enumerables/enumerables.rb
+++ Enumerables/enumerables.rb
@@ -71,21 +71,21 @@ class Array
 
 
     def my_zip(*args)
-        new_arr = Array.new(self.length) {Array.new(args.length + 1)}
-        arr_2d = [self, *args].map {|arr| arr.length}
-        limit = arr_2d.max
-        merged_arr = [self, *args]
         
-        (0...limit).each do |idx1|   
-            (0...limit).each do |idx2|
-                debugger
-                if merged_arr[idx1][idx2] && new_arr[idx1]
-                    new_arr[idx1][idx2] = merged_arr[idx2][idx1] 
-                end 
-               
-            end 
-        end
-    new_arr
+        new_arr = Array.new
+
+        self.length.times {|i|
+    
+            temp_arr = [self[i]]
+
+            args.each {|arr| temp_arr << arr[i]}
+
+            new_arr << temp_arr
+
+        }
+
+        new_arr
+
     end 
 
     # def my_rotate(num=1)
@@ -126,28 +126,28 @@ end
 
 a = [ 4, 5, 6 ]
 b = [ 7, 8, 9 ]
-# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
 p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
-# c = [10, 11, 12]
-# d = [13, 14, 15]
-# [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 
 # arr_2 = [2, [3, 4], 5]
 # arr = [1, 2, 4]
-# # arr.my_each {|ele| p ele + 1}
+# arr.my_each {|ele| p ele + 1}
 
-# # p arr.my_select {|ele|  ele if ele > 2}
+# p arr.my_select {|ele|  ele if ele > 2}
 
-# # p arr.my_reject {|ele| ele > 2}
+# p arr.my_reject {|ele| ele > 2}
 
-# # p arr.my_any? {|ele| ele > 5} # false
-# # p arr.my_any? {|ele| ele > 3} # true
+# p arr.my_any? {|ele| ele > 5} # false
+# p arr.my_any? {|ele| ele > 3} # true
 
-# # p arr.my_all? {|ele| ele < 5} # true
-# # p arr.my_all? {|ele| ele > 3} # false
+# p arr.my_all? {|ele| ele < 5} # true
+# p arr.my_all? {|ele| ele > 3} # false
 
 # p arr_2.my_flatten
 
