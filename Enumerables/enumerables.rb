fe300aedfe9e7856aa40c3c05b43ed290ef7c032
Luis <perezluis167@gmail.com>
2022-11-22 04:40:20 -0500

    Fixed my_rotate

--- Enumerables/enumerables.rb
+++ Enumerables/enumerables.rb
@@ -88,52 +88,52 @@ class Array
 
     end 
 
-    # def my_rotate(num=1)
+    def my_rotate(n=1)
 
-    #     if num * 1 == num
+       if n > 0
+        
+            n.times {self.push(self.shift)}
+            return self
 
-    #         num.times do
-    #             take = self.shift
-    #             self.push(take)
-    #         end
+        elsif n < 0
 
-    #     else
-
-    #         positive = num * -1
-
-    #         positive.times do
-
-    #             remove = self.pop
-    #             self.unshift(remove)
-
-    #         end
-
-    #     end
-
-    #     self
-
-    # end
+            positive_n = n * -1
+            positive_n.times {self.unshift(self.pop)}
+            return self
+            
+        else
+            return self    
+       end
+    end
 
 
 end
 
 
+# my_rotate tests (test 1 at a time for accurate results)
+#-----------------------------------
 
+# a = [ "a", "b", "c", "d" ]
+# p a.my_rotate         #=> ["b", "c", "d", "a"]
+# p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+# p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+# p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
 
+# #my_zip tests
+#-----------------------------------
 
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
-
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
-
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
-
+#-----------------------------------
+# Anything above my_zip; test cases
 
 # arr_2 = [2, [3, 4], 5]
 # arr = [1, 2, 4]
@@ -151,3 +151,4 @@ p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 # p arr_2.my_flatten
 
+#------------------------------------
